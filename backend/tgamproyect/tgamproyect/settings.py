8c79998752787d2089b5952a91f498a71db12738
Franco <franco2662@gmail.com>
2022-07-19 22:35:10 -0400

    Conectar django con db postgres

--- backend/tgamproyect/tgamproyect/settings.py
+++ backend/tgamproyect/tgamproyect/settings.py
@@ -74,9 +74,16 @@ WSGI_APPLICATION = 'tgamproyect.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TGAM',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
