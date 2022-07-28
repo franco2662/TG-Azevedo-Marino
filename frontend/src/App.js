427481c3ae803188dc2c4cff242899098a71e934
Franco <franco2662@gmail.com>
2022-07-27 21:44:42 -0400

    Ajustes para Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import { CssBaseline } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, CssBaseline, Stack } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 function App() {
 
   return (
     <div className="App">
-     <CssBaseline/>
-     <Navbar/>
-     <Sidebar/> 
+      <CssBaseline />
+      <Box>
+        <Navbar />
+        <Stack direction="row">
+          <Sidebar />
+          <Feed />
+        </Stack>
+      </Box>
     </div>
   );
 }
---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Feed = ()=>{
+    return(
+        <Box
+        flex={4}
+        >
+            Feed
+        </Box>
+    )
+}
+
+export default Feed;
