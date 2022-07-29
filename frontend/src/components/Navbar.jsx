ba89a92e455f8c74ce5e1945b303a072716e66e9
Franco <franco2662@gmail.com>
2022-07-28 23:24:13 -0400

    Drawer semi funcional y ajuste de elementos en navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,73 @@
 import React from "react";
-import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Grid } from "@mui/material";
+import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Drawer, IconButton, Stack } from "@mui/material";
 import NotificationsIcon from '@mui/icons-material/Notifications';
-const StyledToolbar = styled(Toolbar)({
-    display: "flex",
-    justifyContent: "space-between"
-})
+import MenuIcon from '@mui/icons-material/Menu';
+import Sidebar from "./Sidebar";
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { useState, useEffect } from "react";
+import { useTheme } from '@mui/material/styles';
+
 const Search = styled("div")(({ theme }) => ({
-    backgroundColor: "white",
-    padding: "0 10px",
-    borderRadius: theme.shape.borderRadius,
-    width: "40%"
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%"
 }))
-const Icons = styled(Box)(({ theme }) => ({
-}))
+
 const Navbar = () => {
-    return (
-        <AppBar position="sticky">
-            <StyledToolbar>
-                <Typography variant="h6">Dashboard</Typography>
-                <Search sx={{ display: { xs: "none", sm: "block" } }} ><InputBase placeholder="search..." /></Search>
-                <Toolbar display="flex">
-                    
-                    <Icons ><Badge badgeContent={4} color="error">
-                        <NotificationsIcon/>
-                    </Badge></Icons>
-                </Toolbar>
-            </StyledToolbar>
-        </AppBar>
-    )
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawerOpen = () => {
+    setOpen(true);
+    console.log('toggleDrawerOpen');
+  };
+  const toggleDrawerClose = () => {
+    setOpen(false);
+    console.log('toggleDrawerClose');
+  };
+  useEffect(() => {
+    setOpen(false);
+    console.log('useEffect called');
+  },[])
+  return (
+    <Box sx={{ display: 'flex',flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar display="flex">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawerOpen}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>Dashboard</Typography>          
+          <Search sx={{ display: { xs: "none", sm: "block" }}} ><InputBase placeholder="search..." /></Search>
+          <Box>
+            <Badge badgeContent={4} color="error">
+            <NotificationsIcon />
+          </Badge>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="persistent" open={open} >
+      <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+        <IconButton onClick={toggleDrawerClose} >
+          <ChevronLeftIcon />
+        </IconButton>
+        </Toolbar>
+        <Sidebar />
+      </Drawer>
+    </Box>
+
+  )
 }
 export default Navbar;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,49 +1,51 @@
 import React from "react";
-import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { AppBar, Toolbar, styled, Typography, Box, InputBase, Badge, Grid, ListItem, ListItemButton, ListItemIcon, ListItemText, tableFooterClasses } from "@mui/material";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import LayersIcon from '@mui/icons-material/Layers';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-const Sidebar = ()=>{
-    return(
-    <Box
+const Sidebar = () => {
+  
+  return (
+    
+      <Box
         flex={1}
         p={2}
-        sx={{display:{xs:"none",sm:"block"}}}   
-    >
-    <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItemButton>
-    </Box>
-    )
+        sx={{ display: { xs: "none", sm: "block" } }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <ShoppingCartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Orders" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Customers" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <BarChartIcon />
+          </ListItemIcon>
+          <ListItemText primary="Reports" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <LayersIcon />
+          </ListItemIcon>
+          <ListItemText primary="Integrations" />
+        </ListItemButton>
+      </Box>
+  )
 }
 
 export default Sidebar;
