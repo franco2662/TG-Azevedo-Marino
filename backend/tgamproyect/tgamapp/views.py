a44a452e6fd63a38c47321766a838dcaf2658fe2
Franco <franco2662@gmail.com>
2022-08-05 13:57:03 -0400

    Request GET para obtener todos los usuarios

--- backend/tgamproyect/tgamapp/views.py
+++ backend/tgamproyect/tgamapp/views.py
@@ -1,10 +1,16 @@
+import imp
+from os import stat
 from django.shortcuts import render
 from .models import *
 from .serializers import *
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'])
 def user_list(request):
     users = Usuario.objects.all()
     serializer = SignInSerializer(users, many=True)
-    return JsonResponse(serializer.data, safe = False)
+    return Response(serializer.data,status.HTTP_200_OK)
