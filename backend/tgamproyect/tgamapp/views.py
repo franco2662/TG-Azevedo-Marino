da7f1f8cf92dd1a428d6189c1fbb9444ebb188ac
Franco <franco2662@gmail.com>
2022-08-05 12:54:48 -0400

    Obteniendo Json de bd con endpoint basico

--- backend/tgamproyect/tgamapp/views.py
+++ backend/tgamproyect/tgamapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from django.http import JsonResponse
 
 # Create your views here.
+def user_list(request):
+    users = Usuario.objects.all()
+    serializer = SignInSerializer(users, many=True)
+    return JsonResponse(serializer.data, safe = False)
--- backend/tgamproyect/tgamproyect/urls.py
+++ backend/tgamproyect/tgamproyect/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tgamapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/',views.user_list)
 ]
