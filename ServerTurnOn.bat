ebefa711ae0cc250c45ea03af1657f4cc7abf2cd
Franco <franco2662@gmail.com>
2023-01-20 00:52:02 -0400

    Venv Bat automatizado

--- ServerTurnOn.bat
+++ ServerTurnOn.bat
@@ -1,3 +1,4 @@
+call CreateVenv.bat
 call backend\venv\Scripts\activate.bat
 call cd backend\tgamproyect
 call python manage.py runserver
---
+++ prueba.bat
@@ -0,0 +1,5 @@
+if not exist %~dp0\backend\venv ( 
+echo hola
+) else (
+echo adios
+)
